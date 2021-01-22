9667da65e52c47d90b610b408d0b8ffd97692a69
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-01-22 11:13:22 -0800

    chore(2020/j5): clean up code

--- 2020/j5.py
+++ 2020/j5.py
@@ -1,11 +1,11 @@
 # This implementation gets 13/15, still looking for ways to get 15/15
 import collections
 
-max_rows = int(input())
-max_cols = int(input())
-
 # Value symbolizing the end of the room.
 END_ROOM_TOKEN = -1
+
+max_rows = int(input())
+max_cols = int(input())
 
 matrix = []
 for _ in range(max_rows):
@@ -17,9 +17,6 @@ matrix[max_rows - 1][max_cols - 1] = END_ROOM_TOKEN
 # Returns an iterator over the cell values that can be jumped to from the given
 # cell value.
 def possible_moves(val):
-    if (max_rows * max_cols) < val:
-        return
-
     # TODO: find the proper range for this using math
     # 1000^2 > 1000000... hacky, but it works so :/
     for possible_row in range(1, 1001):
@@ -39,12 +36,13 @@ def possible_moves(val):
                 yield matrix[matching_col - 1][possible_row - 1]
 
 
-def bfs(row, col):
+# Breadth-first search
+def bfs(start_val):
     visited = set()
-    queue = collections.deque([matrix[row][col]])
+    queue = collections.deque([start_val])
     while queue:
-        val = queue.popleft()
-        for possible_val in possible_moves(val):
+        current_val = queue.popleft()
+        for possible_val in possible_moves(current_val):
             if possible_val == END_ROOM_TOKEN:
                 return True
             if possible_val not in visited:
@@ -54,4 +52,4 @@ def bfs(row, col):
     return False
 
 
-print("yes" if bfs(0, 0) else "no")
+print("yes" if bfs(matrix[0][0]) else "no")
