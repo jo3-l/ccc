dcd80e63d5f7e5b7f8b5be4352cd659ed4a392ff
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-01-29 18:43:14 -0800

    feat(2019/j5): more work

--- 2019/j5.py
+++ 2019/j5.py
@@ -4,17 +4,19 @@ from typing import Dict, Optional, List
 class SubstitutionRule:
     original: str
     replacement: str
+    id: int  # 1-based ID.
 
-    def __init__(self, original, replacement):
+    def __init__(self, id, original, replacement):
+        self.id = id
         self.original = original
         self.replacement = replacement
 
-    def apply(s, index):
-        return s[0:index]+replacement+s[index + len(original):]
+    def apply(self, s, index):
+        return s[0:index] + replacement + s[index + len(self.original) :]
 
 
 class TrieNode:
-    id: Optional[int]  # 1-based ID.
+    id: Optional[int]  # Note that this is 1-based.
     children: Dict[str, "TrieNode"]
     is_leaf: bool
 
@@ -23,50 +25,46 @@ class TrieNode:
         self.id = id
         self.is_leaf = is_leaf
 
-    def unwrap_rule(self):
+    def unwrap_rule_data(self):
         assert self.is_leaf
         return rules[self.id - 1]
 
 
 rules: List[SubstitutionRule] = []
 
-# Root node of the trie.
-root = TrieNode(False)
+root_node = TrieNode(False)
 
-# Construct the trie...
+# IDs are 1-based, so we're ranging from [1, 4) instead of [0, 3).
 for id in range(1, 4):
     original, replacement = input().split(" ")
-    rules.append(SubstitutionRule(original, replacement))
+    rules.append(SubstitutionRule(id, original, replacement))
 
-    node = root
+    node = root_node
     for i in range(len(original)):
         char = original[i]
         # Last character, make the new node a leaf and add the ID.
         if i == len(original) - 1:
+            # We may already have a node at this position - for example, if we
+            # handled the substitution rule 'ABC' and then 'AB' right after. In
+            # this case, we reuse the node, but simply mark it as a leaf.
             if char in node.children:
                 node = node.children[char]
                 node.is_leaf = True
                 node.id = id
-                continue
+            else:
+                # Otherwise, create a new node and set it as a child node.
+                new_node = TrieNode(True, id)
+                node.children[char] = new_node
+                node = new_node
+        else:
+            # Similar logic if we're not at the end, except these aren't leaf nodes,
+            # just normal nodes.
 
-            new_node = TrieNode(True, id)
-            node.children[char] = new_node
-            node = new_node
-            continue
-
-        if char in node.children:
-            node = node.children[char]
-            continue
-
-        new_node = TrieNode(False)
-        node.children[char] = new_node
-        node = new_node
-
-num_substitutions = int(input())
-source, target = input()
-
-def all_substitutions(s):
-    node = root
-    matching_nodes = []
-    for i in range(len(s)):
-        pass
+            # If there's already a node here, we do nothing except update the cursor
+            # as we're not at the end.
+            if char in node.children:
+                node = node.children[char]
+            else:
+                new_node = TrieNode(False)
+                node.children[char] = new_node
+                node = new_node
